bfe31668443e8a8da957d4fde4c14b7b623b8af0
Alex77 <fktrctq321@gmail.com>
2023-02-16 17:00:41 +0700

    poll

---
+++ async-requests/poll/task.js
@@ -0,0 +1,30 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+xhr.responseType = 'json';
+xhr.send();
+
+xhr.onload = () => {
+    if (xhr.status === 200) {
+        let xhrObj = xhr.response;
+        let title = xhrObj.data.title;
+        let answers = xhrObj.data.answers;
+        let titleDiv = document.getElementById('poll__title');
+        titleDiv.textContent = title;
+        let answerParent = document.getElementById('poll__answers');
+        for (let answer of answers) {
+            answerParent.insertAdjacentHTML('afterbegin',
+                ` <button class="poll__answer"> ` + answer + `
+      </button>
+        `
+            );
+        };
+
+        let answersButton = Array.from(document.querySelectorAll('.poll__answer'));
+        answersButton.forEach((item) => {
+            item.onclick = () => {
+                alert('Спасибо, ваш голос засчитан!')
+                return false;
+            };
+        });
+    };
+};
